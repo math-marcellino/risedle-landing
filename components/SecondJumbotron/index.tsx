a59ed8c25af68edc74bef5d2ead9494dd69db6a6
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-10-04 07:56:15 +0700

    chore: clean up code

--- components/SecondJumbotron/index.tsx
+++ components/SecondJumbotron/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 import BackgroundBox from "./BackgroundBox";
 import { SecondJumbotronSection } from "./Section";
 
