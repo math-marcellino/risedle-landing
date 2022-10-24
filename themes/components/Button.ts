1ecf49310b1efdb62384c4585d340358ead79aa8
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-10-24 09:19:42 +0700

    chore: add button hover styling

--- themes/components/Button.ts
+++ themes/components/Button.ts
@@ -35,59 +35,74 @@ const Button: ComponentStyleConfig = {
         'primary.default': {
             color: 'light.neutral.primary',
             bgColor: 'dark.neutral.primary',
+            _hover: { bgColor: 'dark.neutral.soft' },
         },
         'primary.yellow': {
             color: 'light.neutral.primary',
             bgColor: 'semantic.yellow.strong',
+            _hover: { bgColor: 'semantic.yellow.medium' },
         },
         'primary.purple': {
             color: 'light.neutral.primary',
             bgColor: 'semantic.purple.strong',
+            _hover: { bgColor: 'semantic.purple.medium' },
         },
         'primary.green': {
             color: 'light.neutral.primary',
             bgColor: 'semantic.green.strong',
+            _hover: { bgColor: 'semantic.green.medium' },
         },
         'primary.red': {
             color: 'light.neutral.primary',
             bgColor: 'semantic.red.strong',
+            _hover: { bgColor: 'semantic.red.medium' },
         },
         // Secondary Type
         'secondary.default': {
             color: 'dark.neutral.primary',
             bgColor: 'dark.background.subtle',
+            _hover: { color: 'dark.neutral.soft' },
         },
         'secondary.yellow': {
             color: 'semantic.yellow.strong',
             bgColor: 'semantic.yellow.dim',
+            _hover: { color: 'semantic.yellow.medium' },
         },
         'secondary.purple': {
             color: 'semantic.purple.strong',
             bgColor: 'semantic.purple.dim',
+            _hover: { color: 'semantic.purple.medium' },
         },
         'secondary.green': {
             color: 'semantic.green.strong',
             bgColor: 'semantic.green.dim',
+            _hover: { color: 'semantic.green.medium' },
         },
         'secondary.red': {
             color: 'semantic.red.strong',
             bgColor: 'semantic.red.dim',
+            _hover: { color: 'semantic.red.medium' },
         },
         // Ghost Type
         'ghost.default': {
             color: 'dark.neutral.primary',
+            _hover: { bgColor: 'dark.background.subtle' },
         },
         'ghost.yellow': {
             color: 'semantic.yellow.strong',
+            _hover: { bgColor: 'semantic.yellow.dim' },
         },
         'ghost.purple': {
             color: 'semantic.purple.strong',
+            _hover: { bgColor: 'semantic.purple.dim' },
         },
         'ghost.green': {
             color: 'semantic.green.strong',
+            _hover: { bgColor: 'semantic.green.dim' },
         },
         'ghost.red': {
             color: 'semantic.red.strong',
+            _hover: { bgColor: 'semantic.red.dim' },
         },
     },
 };
